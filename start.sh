3e34b3ceef4d9ad765a5119cde5ff2af0a7346b1
Etchial <98379633+Etchial@users.noreply.github.com>
2022-12-29 22:33:04 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Greymattersbot/EvaMaria /EvaMaria
+  git clone https://github.com/Etchial/Movie-Search-File /Movie-Search-File
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /Movie-Search-File
 fi
-cd /EvaMaria
+cd /Movie-Search-File
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
