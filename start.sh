1ab71ee8795eede7dc407e0b80ec85bd932b1a9f
Etchial <98379633+Etchial@users.noreply.github.com>
2023-01-06 13:43:16 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Etchial/Movie-Search-File /Movie-Search-File
+  git clone https://github.com/Etchial/Movie-File-search /Movie-File-search
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Movie-Search-File
+  git clone $UPSTREAM_REPO /Movie-File-search
 fi
-cd /Movie-Search-File
+cd /Movie-File-search
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
